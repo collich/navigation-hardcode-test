2fa227da8f7c2f53f61704cb9f07f05a0160a529
Collin <collin-chua@hotmail.com>
2023-02-10 17:05:38 +0800

    restart

--- src/Hooks/useNavigationContext.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react"
-import NavigationContext from "../context/navigation"
-
-const useNavigationContext = () => {
-  return useContext(NavigationContext)
-}
-
-export default useNavigationContext
--- src/components/Route.js
+++
@@ -1,8 +0,0 @@
-import useNavigationContext from "../Hooks/useNavigationContext"
-
-const Route = ({path, children}) => {
-    const {pathName} = useNavigationContext()
-  return path === pathName ? children : null;
-}
-
-export default Route
